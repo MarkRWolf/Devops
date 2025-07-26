db5843031a80777eb697604e9a7e919bc391619a
Mark Wolf <74432210+MarkRWolf@users.noreply.github.com>
2025-07-26 18:57:43 +0200

    ssr dashboard nav

--- client/src/app/(store)/dashboard/stats/page.tsx
+++ client/src/app/(store)/dashboard/stats/page.tsx
@@ -1,18 +1,17 @@
 export const dynamic = "force-dynamic";
-
-import { checkAuth } from "@/lib/helpers/checkAuth";
-import { fetchWorkflowRuns } from "@/lib/github/helpers";
-import WorkflowRuns from "@/components/github/WorkflowRuns";
 import Charts from "@/components/charts/Charts";
 import DashboardNav from "@/components/dashboardNav/DashboardNav";
+import WorkflowRuns from "@/components/github/WorkflowRuns";
+import { fetchWorkflowRuns } from "@/lib/github/helpers";
+import { checkAuth } from "@/lib/helpers/checkAuth";
 
-export default async function DashboardHome() {
+export default async function DashboardStats() {
   await checkAuth();
   const workflowRuns = await fetchWorkflowRuns();
 
   return (
     <div>
-      <DashboardNav slug="/dashboard" />
+      <DashboardNav slug="/dashboard/stats" />
       <div>
         <Charts workflowRuns={workflowRuns} />
         <WorkflowRuns runs={workflowRuns} />
---
+++ client/src/components/dashboardNav/DashboardNav.tsx
@@ -0,0 +1,27 @@
+import BetterLink from "@/components/BetterLink";
+
+const routes = [
+  { href: "/dashboard", label: "Overview" },
+  { href: "/dashboard/stats", label: "Stats" },
+];
+
+export default async function DashboardNav({ slug }: { slug: string }) {
+  return (
+    <nav className="container-main mx-auto flex gap-4 border-b border-gray-200 mb-6">
+      {routes.map(({ href, label }) => {
+        const isActive = slug === href;
+        return (
+          <BetterLink
+            key={href}
+            href={href}
+            className={`pb-2 border-b-2 ${
+              isActive ? "text-blue-500" : "border-transparent text-gray-500 hover:text-black"
+            }`}
+          >
+            {label}
+          </BetterLink>
+        );
+      })}
+    </nav>
+  );
+}
