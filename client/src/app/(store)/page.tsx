c2a9e01d82e3e729b6d63c6d9cb5f7f8eeb40af3
Mark Wolf <74432210+MarkRWolf@users.noreply.github.com>
2025-07-30 01:49:12 +0200

    test

--- client/src/app/(store)/page.tsx
+++ client/src/app/(store)/page.tsx
@@ -10,7 +10,7 @@ export default async function Home() {
   const projectWorkflowRuns = await fetchWorkflowRuns("/project");
 
   return (
-    <div className="flex flex-col items-center gap-4 mt-8">
+    <div className="flex flex-col items-center gap-4 mt-10">
       <h1 className="text-xl">Welcome to my Devops Dashboard</h1>
       <Charts workflowRuns={projectWorkflowRuns} />
       <WorkflowUpdatesProvider initialRun={projectWorkflowRuns[0]}>
