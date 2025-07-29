9e18bb3266d8acffea0e4f48ca7e80ce15c7abdd
Mark Wolf <74432210+MarkRWolf@users.noreply.github.com>
2025-07-29 16:19:21 +0200

    maybe provider works too?

--- client/src/components/github/WorkflowRuns.tsx
+++ client/src/components/github/WorkflowRuns.tsx
@@ -1,26 +1,32 @@
+"use client";
 import { GitHubWorkflowRun } from "@/lib/github/models";
 import WorkflowRunDetails from "./WorkflowRunDetails";
+import { useWorkflowUpdates } from "./WorkflowUpdatesProvider";
 
 interface WorkflowRunsProps {
   runs: GitHubWorkflowRun[];
   project?: string;
 }
 
-const WorkflowRuns = async ({ runs, project = "" }: WorkflowRunsProps) => {
+const WorkflowRuns = ({ runs, project = "" }: WorkflowRunsProps) => {
+  const { lastRun } = useWorkflowUpdates();
+  const merged = lastRun && lastRun.id !== runs[0]?.id ? [lastRun, ...runs] : runs;
+
   return (
     <div className="container-main py-6">
       <h2 className="text-2xl font-semibold mb-8 text-center">Workflow Runs</h2>
-      {runs.length > 0 && (
+      {merged.length > 0 ? (
         <div className="max-w-7xl mx-auto py-4 px-8 border space-y-2">
           <h2 className="text-lg font-semibold mb-4">Latest Project Workflow Runs:</h2>
           <div className="flex flex-col gap-4">
-            {runs.map((run) => (
+            {merged.map((run) => (
               <WorkflowRunDetails key={run.id} run={run} urlInsert={project} />
             ))}
           </div>
         </div>
+      ) : (
+        <p>No project workflow runs found or configured.</p>
       )}
-      {runs.length === 0 && <p>No project workflow runs found or configured.</p>}
     </div>
   );
 };
