fb00c5127fb509b26117a54e46a9cea73bd8ef27
Mark Wolf <74432210+MarkRWolf@users.noreply.github.com>
2025-08-01 19:18:47 +0200

    try again

--- client/src/lib/helpers/checkAuth.ts
+++ client/src/lib/helpers/checkAuth.ts
@@ -1,9 +1,10 @@
 // @/lib/helpers/checkAuth.ts
 import { cookies } from "next/headers";
 import type { User } from "@/lib/user/user";
+import { baseUrl } from "../settings";
 
 export async function checkAuth(): Promise<User | null> {
-  const res = await fetch(`/api/account/me`, {
+  const res = await fetch(`${baseUrl}/api/account/me`, {
     headers: { cookie: (await cookies()).toString() },
     cache: "no-store",
   });
