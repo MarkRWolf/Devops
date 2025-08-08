0d27931799c1bd8cc0a5ea17d8e7a89be24273d5
Mark Wolf <74432210+MarkRWolf@users.noreply.github.com>
2025-08-08 02:33:19 +0200

    Update Charts.tsx

--- client/src/components/charts/Charts.tsx
+++ client/src/components/charts/Charts.tsx
@@ -9,7 +9,7 @@ const Charts = ({ workflowRuns }: { workflowRuns?: GitHubWorkflowRun[] }) => {
 
   return (
     <div className="py-6">
-      <h2 className="text-2xl font-semibold mb-8 text-center">Charts</h2>
+      {/* <h2 className="text-2xl font-semibold mb-8 text-center">Charts</h2> */}
 
       <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 justify-items-center gap-2 gap-y-12">
         <WorkflowCharts workflowRuns={workflowRuns} />
