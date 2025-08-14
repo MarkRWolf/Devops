8a22d72f88ae9a0c6613004f6cf76a9d5e0c4ac3
Mark Wolf <74432210+MarkRWolf@users.noreply.github.com>
2025-08-14 19:29:41 +0200

    metadata

--- client/src/app/(store)/layout.tsx
+++ client/src/app/(store)/layout.tsx
@@ -1,7 +1,16 @@
 // client/src/app/(store)/layout.tsx
+import { Metadata } from "next";
 import "../globals.css";
 import { ThemeProvider } from "./themeProvider";
 import Header from "@/components/header/Header";
+
+export const metadata: Metadata = {
+  title: "DevOptics",
+  description: "A DevOps dashboard for monitoring CI/CD pipelines",
+  icons: {
+    icon: "/logoWhite.png",
+  },
+};
 
 interface LayoutProps {
   children: React.ReactNode;
